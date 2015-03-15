3d86acd4bdcc9fb6cb7ece3286d7265adaddee6c
khanomgol <ladangol@buffalo.edu>
2015-03-15 18:06:54 -0400

    Just a test for Hello world

---
+++ src/ladangol/edu/buffalo/cse/Demo.java
@@ -0,0 +1,11 @@
+package ladangol.edu.buffalo.cse;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello CSE531");
+
+	}
+
+}
